4938a78454fba9a7beee4f85208f173bfd7ff7fa
Rita C. L. Nascimento <sarahsetej@gmail.com>
2022-01-15 20:47:37 -0300

    Props saved

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ function Listing () {
 });
 
     useEffect (() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
                 .then(response => {
                     const data = response.data as MoviePage;
                     setPage(data);
@@ -39,7 +39,7 @@ function Listing () {
             <div className="container">
                 <div className="row">
                     {page.content.map (movie => (
-                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                             <MovieCard movie = {movie}/>
                         </div>
                     )
